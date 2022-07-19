0e0d5c25c4dabb69c5c862f5d5ba6055d16fb86f
Nimah <busayonimota1603@gmail.com>
2022-07-19 20:09:33 +0100

    Implements the selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+
+/**
+ * swap - switch position of the elements passed
+ *
+ * @x: pointer the first element
+ * @y: pointer the second element
+ */
+
+void swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * selection_sort - sorts an array by comparing an element
+ * at an index with everyother elements
+ * in an array of size n
+ * @array: array to sort
+ * @size: size of the array
+ * Return: return void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, jmin;
+
+	if (size < 2)
+	{
+		return;
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		jmin = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] > array[jmin])
+				jmin = j;
+		}
+		if (jmin != i)
+		{
+			swap(&array[i], &array[jmin]);
+			print_array(array, size);
+		}
+	}
+}
