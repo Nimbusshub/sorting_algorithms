cd8c1ed96b342d3be6ab53a092aa58aaf4f66567
Nimah <busayonimota1603@gmail.com>
2022-07-19 20:18:26 +0100

    [Fix] remove the bug and update the algo

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -39,7 +39,7 @@ void selection_sort(int *array, size_t size)
 		jmin = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] > array[jmin])
+			if (array[j] < array[jmin])
 				jmin = j;
 		}
 		if (jmin != i)
