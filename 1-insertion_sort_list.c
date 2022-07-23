c8dd24b011ba332c22ba727f7f487ebb28e9cd4d
Nimah <busayonimota1603@gmail.com>
2022-07-23 12:35:03 +0100

    [Fix] the bug in the insertion sort algo

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,52 @@
 #include "sort.h"
-#include <stdio.h>
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @head: A pointer to the head of the doubly-linked list.
+ * @node1: A pointer to the first node to swap.
+ * @node2: The second node to swap.
+ */
+
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*head = node2;
+
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp = *list;
-	int value;
-	
-	if (*list == NULL || (*list)->next == NULL)
+	listint_t *iter, *insert, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-		
-	temp = temp->next;
-	/* {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};*/
-	
-	while (temp != NULL)
+
+	for (iter = (*list)->next; iter != NULL; iter = temp)
 	{
-		value = temp->n;
-		printf("\n %d \n", value);
-		
-		while (temp->prev != NULL && temp->prev->n > value)
+		temp = iter->next;
+		insert = iter->prev;
+
+		while (insert != NULL && iter->n < insert->n)
 		{
-			printf("\t [%d]\n", temp->prev->n);
-			temp =  temp->prev;
-			printf("\t %d \n", temp->prev->n);
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)(*list));
 		}
-		temp = temp->next;
 	}
 }
-
-
-
-
-void nodes_swap(listint_t **list, listint_t *node_1, listint_t *node_2)
-{
-	listint_t *temp = node_1;
-	node_1->next = node_2->next;
-	node_1->prev = temp->next;
-	node_2->next = node_2->prev;
-	node_2->prev = temp->prev;
-	if (node_1->next != NULL)
-		node_1->next->next = node_2->next;
-		
-	if (node_2->prev == NULL)
-	{
-		*list = node_2;
-	}
-}
-
